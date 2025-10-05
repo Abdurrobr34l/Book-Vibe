637f96d9e8bd48719a2091bc4d29639619a1f6cb
AbdurRob <abdurrobr34l@gmail.com>
2025-10-06 00:50:54 +0600

    {/* Added favicon, installed DaisyUI, installed React Router */}

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <h1>I am from router</h1>,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
