457ef9562d573c3f470472d939e206b11f7201dd
AbdurRob <abdurrobr34l@gmail.com>
2025-10-06 02:02:39 +0600

    Header resposive done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const navigationData = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "Listed Books", path: "/listed-books" },
+    { id: 3, name: "Pages to Read", path: "/pages-to-read" },
+  ];
+
+  // Tailwind classes for active and normal links
+  const activeClass = "text-[#23BE0A] border-1 border-[#23BE0A] bg-transparent";
+  const normalClass = "text-[#131313] hover:text-[#23BE0A]";
+
   return (
-    <div>
-      <h2 className='text-center'>HEADER</h2>
-    </div>
+    <header className="navbar py-5 px-0">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn-ghost pr-5 lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52"
+          >
+            {navigationData.map((route) => (
+              <li key={route.id}>
+                <NavLink
+                  to={route.path}
+                  className={({ isActive }) =>
+                    isActive ? activeClass : normalClass
+                  }
+                >
+                  {route.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <a className="btn-ghost text-[28px] font-bold">Books Vibe</a>
+      </div>
+
+      <nav className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal">
+          {navigationData.map((route) => (
+            <li key={route.id}>
+              <NavLink
+                to={route.path}
+                className={({ isActive }) =>
+                  isActive ? activeClass : normalClass
+                }
+              >
+                {route.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className="navbar-end gap-5">
+        <a className="btn bg-[#23BE0A] shadow-none border-none">Sign In</a>
+        <a className="btn bg-[#59C6D2] shadow-none border-none">Sign Up</a>
+      </div>
+    </header>
   );
 };
 
--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 import { Outlet } from 'react-router';
 import Footer from '../../Components/Footer/Footer';
+import Container from '../../Components/Container/Container';
 
 const Root = () => {
   return (
     <div>
+      <Container>
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
+      </Container>
     </div>
   );
 };
